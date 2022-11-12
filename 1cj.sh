c0e238c6c3e48b8914563858f17ac63f532ba9be
Max Hillebrand <30683012+MaxHillebrand@users.noreply.github.com>
2022-11-12 19:51:48 +0100

    start coinjoin on script start

--- 1cj.sh
+++ 1cj.sh
@@ -1,5 +1,8 @@
 `#!/usr/bin/env bash`
 
+./wcli.sh selectwallet $1
+./wcli.sh startcoinjoin $1 true true
+
 ( tail -f -n0 ~/.walletwasabi/client/Logs.txt & ) | grep -q "Broadcasted. Coinjoin TxId"
 
 ./wcli.sh selectwallet $1
