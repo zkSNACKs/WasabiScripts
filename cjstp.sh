50bd39cd263ba077cdd69b0abcd0fc08db490334
Max Hillebrand <30683012+MaxHillebrand@users.noreply.github.com>
2022-11-12 19:30:13 +0100

    simplify cjstp

--- cjstp.sh
+++ cjstp.sh
@@ -1,18 +1,8 @@
 `#!/usr/bin/env bash`
 
-for ARGUMENT in "$@"
-do
-	KEY=$(echo $ARGUMENT | cut -f1 -d=)
-
-	KEY_LENGTH=${#KEY}
-	VALUE="${ARGUMENT:$KEY_LENGTH+1}"
-
-	export "$KEY"="$VALUE"
-done
-
 ( tail -f -n0 ~/.walletwasabi/client/Logs.txt & ) | grep -q "Broadcasted. Coinjoin TxId"
 
-./wcli.sh selectwallet $wallet
+./wcli.sh selectwallet $1
 ./wcli.sh stopcoinjoin
 
 echo "Coinjoin was successful - coinjoining stopped"
